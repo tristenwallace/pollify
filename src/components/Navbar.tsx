4840a069ee8d134573e297f03cbefe86e243e9f3
tristenwallace <tri3sten@gmail.com>
2024-04-16 19:47:36 -0400

    feat: Style Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { logout } from '../features/usersSlice';
 import { AppDispatch, RootState } from '../app/store';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 
 const Navbar = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -16,16 +16,26 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/create">Create Poll</Link>
-      <Link to="/leaderboard">Leaderboard</Link>
-      {isAuthenticated && (
-        <>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      )}
-    </nav>
+    <AppBar position="static" color="secondary" elevation={0}>
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={RouterLink} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/create">
+            Create Poll
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/leaderboard">
+            Leaderboard
+          </Button>
+        </Box>
+        {isAuthenticated && (
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
