6884cb05413938066e945bb97565006d5afd348c
tristenwallace <tri3sten@gmail.com>
2024-04-23 21:48:14 -0400

    fix: Add port to sequelize connection

--- server/src/config/sequelize.ts
+++ server/src/config/sequelize.ts
@@ -4,21 +4,15 @@ import Config from './config';
 const env = process.env.NODE_ENV || 'development';
 const config = Config[env];
 
-console.log(`seq-env: ${env}`);
-console.log(`seq-config-database: ${config.database}`);
-console.log(`seq-config-username: ${config.username}`);
-console.log(`seq-config-password: ${config.password}`);
-console.log(`seq-config-dialect: ${config.dialect}`);
-console.log(`seq-config-host: ${config.host}`);
-
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   {
     host: config.host,
+    port: config.port,
     dialect: config.dialect,
-    logging: console.log,
+    logging: false,
   },
 );
 
