e42aa31752a1bca4ad50be1cb01e1bb72d709335
tristenwallace <tri3sten@gmail.com>
2024-04-30 12:45:00 -0400

    feat: Mobile Navbar with login/signup

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,47 +1,88 @@
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { logout } from '../features/usersSlice';
 import { AppDispatch, RootState } from '../app/store';
-import { AppBar, Toolbar, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
   const dispatch: AppDispatch = useDispatch();
-  const currentUser = useSelector(
-    (state: RootState) => state.users.currentUser,
-  );
+  const currentUser = useSelector((state: RootState) => state.users.currentUser);
+  const navigate = useNavigate();
+
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState<null | HTMLElement>(null);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
   const handleLogout = () => {
     dispatch(logout());
+    handleMobileMenuClose();
   };
+
+  const handleLoginSignup = () => {
+    navigate('/login');
+  };
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem component={RouterLink} to="/" onClick={handleMobileMenuClose}>
+        Home
+      </MenuItem>
+      <MenuItem component={RouterLink} to="/create" onClick={handleMobileMenuClose}>
+        Create Poll
+      </MenuItem>
+      <MenuItem component={RouterLink} to="/leaderboard" onClick={handleMobileMenuClose}>
+        Leaderboard
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <AppBar position="static" color="secondary" elevation={0}>
       <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Button color="inherit" component={RouterLink} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/create">
-            Create Poll
-          </Button>
-          <Button color="inherit" component={RouterLink} to="/leaderboard">
-            Leaderboard
-          </Button>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2, display: { xs: 'block', md: 'none' } }}
+          onClick={handleMobileMenuOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Button color="inherit" component={RouterLink} to="/">Home</Button>
+          <Button color="inherit" component={RouterLink} to="/create">Create Poll</Button>
+          <Button color="inherit" component={RouterLink} to="/leaderboard">Leaderboard</Button>
         </Box>
         {currentUser ? (
           <Button color="inherit" onClick={handleLogout}>
             Logout
           </Button>
         ) : (
-          <>
-            <Button color="inherit" component={RouterLink} to="/login">
-              Login
-            </Button>
-            <Button color="inherit" component={RouterLink} to="/signup">
-              Signup
-            </Button>
-          </>
+          <Button color="inherit" onClick={handleLoginSignup} sx={{ display: { xs: 'block', sm: 'block' } }}>
+            Login/Signup
+          </Button>
         )}
       </Toolbar>
+      {renderMobileMenu}
     </AppBar>
   );
 };
