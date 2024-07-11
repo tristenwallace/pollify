152d31598c4562e328334e47377f243d2efb557d
tristenwallace <tri3sten@gmail.com>
2024-07-11 14:28:08 -0400

    refactor Home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link as RouterLink } from 'react-router-dom';
 import { fetchPolls } from '../features/pollSlice';
-import PollList from './Polls/PollList';
 import PollPagination from './Polls/PollPagination';
 import { AppDispatch, RootState } from '../store/store';
 import {
@@ -16,7 +14,7 @@ import {
 const Home = () => {
   const dispatch: AppDispatch = useDispatch();
   const [showAnswered, setShowAnswered] = useState(false);
-  const user = useSelector((state: RootState) => state.users.currentUser);
+  const user = useSelector((state: RootState) => state.users.currentUser!); // Ensure that user is always defined
   const polls = useSelector((state: RootState) => state.poll.polls);
   const pollStatus = useSelector((state: RootState) => state.poll.status);
   const error = useSelector((state: RootState) => state.poll.error);
@@ -30,6 +28,7 @@ const Home = () => {
 
   // Show loading indicator during data fetch
   if (pollStatus === 'loading') return <CircularProgress />;
+
   // Display error if data fetch fails
   if (error) return <Typography>Error: {error}</Typography>;
 
@@ -37,82 +36,21 @@ const Home = () => {
   const answeredPolls = Object.values(polls).filter(
     poll => poll.votes && poll.votes.some(vote => vote.userId === user?.id),
   );
+
   const unansweredPolls = Object.values(polls).filter(
     poll => poll.votes && !poll.votes.some(vote => vote.userId === user?.id),
   );
 
-  if (!user) {
-    // When there is no user logged in, only display 5 most voted on polls
-    const topPolls = Object.values(polls)
-      .sort((a, b) => b.votes.length - a.votes.length)
-      .slice(0, 3);
-
-    return (
-      <Container>
-        <div
-          style={{
-            padding: '20px',
-            background: '#f0f0f0',
-            borderRadius: '8px',
-            marginTop: '20px',
-          }}
-        >
-          <Typography variant="h5" gutterBottom>
-            Welcome to Employee Polls!
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            A fun and engaging way to create and participate in polls. Get
-            started by signing up or logging in!
-          </Typography>
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              component={RouterLink}
-              to="/signup"
-              sx={{ mr: 2, mt: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              component={RouterLink}
-              to="/login"
-              sx={{ mr: 2, mt: 2 }}
-            >
-              Login
-            </Button>
-          </div>
-        </div>
-        <Typography variant="h4" sx={{ my: 4 }}>
-          Featured Polls
-        </Typography>
-        {topPolls.length ? (
-          <PollList polls={topPolls} />
-        ) : (
-          <Typography>No polls available.</Typography>
-        )}
-      </Container>
-    );
-  }
-
   return (
     <Container>
-      <div
-        style={{
-          padding: '20px',
-          borderRadius: '8px',
-          marginTop: '20px',
-        }}
-      >
+      <Box sx={{ padding: '20px', borderRadius: '8px', marginTop: '20px' }}>
         <Typography variant="h5" gutterBottom>
           Welcome, {user.name}!
         </Typography>
         <Typography variant="body1" gutterBottom>
           Take a look at the most recent polls!
         </Typography>
-      </div>
+      </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
         <Button
           variant="contained"
